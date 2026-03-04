cd94953dd7edbea7d4d5d7e0789e974242c8e318
Horn Vanhong <vanhong.horn@rhbgroup.com.kh>
2026-03-04 22:26:14 +0700

    Update, Search

--- src/main/java/org/sr_g3/view/ProgramUi.java
+++ src/main/java/org/sr_g3/view/ProgramUi.java
@@ -3,19 +3,10 @@ package org.sr_g3.view;
 import org.sr_g3.config.app.StockManagementSystem;
 import org.sr_g3.dao.StockManagementDao;
 import org.sr_g3.dao.StockManagementDaoImpl;
-import org.sr_g3.model.Product;
 import org.sr_g3.utils.Colors;
 import org.sr_g3.utils.Console;
 import org.sr_g3.utils.ProductTableDesign;
 import org.sr_g3.utils.Validator;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
-
 public class ProgramUi {
 
     static StockManagementDao stockManagementDao = new StockManagementDaoImpl();
@@ -34,7 +25,12 @@ public class ProgramUi {
 
             System.out.println();
             Console.print("Stock Management System", "=", 57, Colors.GREEN, Colors.BLUE);
-            ProductTableDesign.printTable(stockManagementDao.fetchStock(limit,offset), currentPage, totalPages);
+            ProductTableDesign.printTable(
+                    stockManagementDao.fetchStock(limit, offset),
+                    currentPage,
+                    totalPages,
+                    totalRecords
+            );
             Console.displayTableMenu();
 
             inputMenuBlock:
@@ -82,108 +78,18 @@ public class ProgramUi {
 
                     // Write
                     case "W" -> {
-                        System.out.println();
-                        StockManagementDao dao = new StockManagementDaoImpl();
-                        long nextId = dao.getNextProductId();
-
-                        if (nextId <= 0) {
-                            Console.printErrorMessage("Could not determine next product ID.");
-                            break inputMenuBlock;
-                        }
-
-                        System.out.println("ID: " + nextId);
-
-                        try {
-                            String name = Console.input(
-                                    "Input Product Name : ",
-                                    "[A-Za-z0-9\\s\\-\\.\\,]{3,100}",
-                                    "Invalid name! (3-100 chars, letters/numbers/space/-.,)"
-                            ).trim();
-
-
-                            String priceStr = Console.input(
-                                    "Enter price: ",
-                                    Validator.floatRule(),
-                                    "Invalid price! Enter positive number"
-                            );
-                            double price = Double.parseDouble(priceStr);
-                            if (price <= 0) {
-                                Console.printErrorMessage("Price must be > 0");
-                                break inputMenuBlock;
-                            }
-
-
-                            String qtyStr = Console.input(
-                                    "Enter quantity: ",
-                                    Validator.numberRule(),
-                                    "Invalid quantity! Enter non-negative integer"
-                            );
-                            int quantity = Integer.parseInt(qtyStr);
-                            if (quantity < 0) {
-                                Console.printErrorMessage("Quantity cannot be negative");
-                                break inputMenuBlock;
-                            }
-
-                            Product newProduct = new Product();
-                            newProduct.setProduct_id(nextId);
-                            newProduct.setName(name);
-                            newProduct.setUnit_price(price);
-                            newProduct.setQuantity(quantity);
-                            newProduct.setImported_date(LocalDate.now());
-
-                            StockManagementSystem.addPendingInsert(newProduct);
-
-
-
-                        } catch (Exception e) {
-                            Console.printErrorMessage("Error: " + e.getMessage());
-                        }
-
-                        System.out.println("Enter to continue.....");
-                        new Scanner(System.in).nextLine();
-
+                        system.write();
                         break inputMenuBlock;
                     }
                     // Read (id)
                     case "R" -> {
-                        System.out.println();
-
-                        String idStr = Console.input(
-                                "Please input id to get record : ",
-                                Validator.numberRule(),
-                                "Please enter a valid positive number"
-                        );
-
-                        long id;
-                        try {
-                            id = Long.parseLong(idStr);
-                            if (id <= 0) {
-                                Console.printErrorMessage("ID must be a positive number.");
-                                break inputMenuBlock;
-                            }
-                        } catch (NumberFormatException e) {
-                            Console.printErrorMessage("Invalid ID format.");
-                            break inputMenuBlock;
-                        }
-
-                        StockManagementDao dao = new StockManagementDaoImpl();
-                        Optional<Product> optionalProduct = dao.getProductById(id);
-
-                        if (optionalProduct.isPresent()) {
-                            ProductTableDesign.printSingleProduct(optionalProduct.get());
-                        } else {
-                            Console.printErrorMessage("No product found with ID: " + id);
-                        }
-
-                        System.out.println("\nEnter to continue...");
-                        new Scanner(System.in).nextLine();
-
+                        system.readById();
                         break inputMenuBlock;
                     }
 
                     // Update
                     case "U" -> {
-                        System.out.println("update");
+                        system.updateProduct();
                         break inputMenuBlock;
                     }
 
@@ -195,7 +101,7 @@ public class ProgramUi {
 
                     // Search (name)
                     case "S" -> {
-                        System.out.println("search-by-name");
+                        system.searchByName();
                         break inputMenuBlock;
                     }
 
